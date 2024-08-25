5d8b3fce0d8cb476860818fcfc65d2712b20efc2
HadiKa47 <Hade.Kary24@gmail.com>
2024-08-25 16:17:18 +0300

    verifyEmail Endpoint

--- api/mailtrap/emails.js
+++ api/mailtrap/emails.js
@@ -23,3 +23,25 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${error}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "31e68ed2-5a75-408c-80aa-06fabcfc4216",
+      template_variables: {
+        company_info_name: "Scorpion Company",
+        name: name,
+      },
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
